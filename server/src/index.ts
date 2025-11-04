4c5e9546b1802c527d2c685512d2aad311734d42
ahmed-elhdad <ahalhdad2023@gmail.com>
2025-11-04 22:51:19 +0200

    Finish my server

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoutes";
 import chalengeRoutes from "./routes/chalengeRoutes";
 const app = express();
 import dotenv from "dotenv";
-dotenv.config({path:".env.local"});
+dotenv.config({ path: ".env.local" });
 const PORT = 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -34,7 +34,3 @@ connectDB();
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
-
-
---
+++ server/src/swagger.ts
@@ -0,0 +1,23 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Fitness Tracker Server",
+      version: "1.0.0",
+      description: "Docs for my backend API",
+    },
+    servers: [
+      {
+        url: process.env.BASE_URL,
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export { swaggerUi, swaggerSpec };
